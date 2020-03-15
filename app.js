f51c29b6a3f95300b57a520a0abf367d1f93415d
Brett Poarch <brett.poarch@gmail.com>
2020-03-15 13:20:06 -0700

    updated formatting

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const display = document.querySelector('#root');
-const button = document.querySelector('#quote-generator');
+const display = document.querySelector('#text');
+const button = document.querySelector('#new-quote');
 const body = document.querySelector('body');
 const twitter = document.querySelector('#tweet-quote');
+const twitterLink = document.querySelector('.fa');
+const author = document.querySelector('#author');
 
-// https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand
+
 
 function setColor(){
 	const red = Math.floor(Math.random() * 256);
@@ -11,7 +13,13 @@ function setColor(){
 	const blue = Math.floor(Math.random() * 256);	
 	const rgb = (red + ', ' + green + ', ' + blue);
 	
-	return body.style.backgroundColor = `rgb(${rgb})`
+	return (
+		body.style.backgroundColor = `rgb(${rgb})`,
+		twitterLink.style.color = `rgb(${rgb})`,
+		button.style.backgroundColor = `rgb(${rgb})`,
+		display.style.color = `rgb(${rgb})`,
+		author.style.color = `rgb(${rgb})`
+		)
 
 };
 
@@ -25,16 +33,18 @@ async function getQuote() {
 async function render(){
 	const quotes = await getQuote();
 	const quote = quotes.message;
-	console.log(quote)
-	
+	const newAuthor = "Donald Trump"
+
 	twitter.addEventListener('click', function(){
 		twitter.href = `https://twitter.com/intent/tweet?text=${quote} - Donald Trump`
 	});
 
 
-	display.innerHTML = `<div>
+	display.innerHTML = `<div">
 		${quote}
-		- Donald Trump
+	</div>`
+	author.innerHTML = `<div>
+		- ${newAuthor}
 	</div>`
 };
 
